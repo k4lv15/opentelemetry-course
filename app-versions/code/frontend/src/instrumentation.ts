7b729ace3cfbe3a8ea96f9c0488b8508d38bb17d
Lauro Fialho Müller <contact@lauromueller.com>
2026-03-03 15:51:22 +0100

    feat: implement lab-traces-auto.md

--- app-versions/code/frontend/src/instrumentation.ts
+++ app-versions/code/frontend/src/instrumentation.ts
@@ -1,10 +1,14 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
 const sdk = new NodeSDK({
   serviceName: 'translation-frontend',
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://otel-collector:4318/v1/traces',
+  }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
       url: 'http://otel-collector:4318/v1/metrics',
